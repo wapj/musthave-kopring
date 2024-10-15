3efe5d45fb51d76edc226381c2957fbd1a4674b3
wapj <wapj2000@gmail.com>
2024-10-16 00:29:14 +0900

    1장 헬로 스트링 부트

---
+++ andybooks/src/main/kotlin/gyus/kopring/andybooks/AndybooksApplication.kt
@@ -0,0 +1,11 @@
+package gyus.kopring.andybooks
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AndybooksApplication
+
+fun main(args: Array<String>) {
+	runApplication<AndybooksApplication>(*args)
+}
