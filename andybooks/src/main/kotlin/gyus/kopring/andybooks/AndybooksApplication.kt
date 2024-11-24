664875dfdd7e6cbddee54c0f81e3d8437a2cdb22
wapj <wapj2000@gmail.com>
2024-11-25 00:04:11 +0900

    [4.2] Environment 인터페이스 활용

--- andybooks/src/main/kotlin/gyus/kopring/andybooks/AndybooksApplication.kt
+++ andybooks/src/main/kotlin/gyus/kopring/andybooks/AndybooksApplication.kt
@@ -1,34 +1,23 @@
 package gyus.kopring.andybooks
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.core.env.Environment
 
 @SpringBootApplication
-class AndybooksApplication: CommandLineRunner {
-	@Value("\${info.app.name}")
-	lateinit var appName: String
+class AndybooksApplication(val environment: Environment) {
 
-	@Value("\${info.app.version}")
-	lateinit var appVersion: String
-
-	@Value("\${info.app.description}")
-	lateinit var appDescription: String
-
-	override fun run(vararg args: String?) {
-		println("이름 : $appName")
-		println("버전 : $appVersion")
-		println("설명 : $appDescription")
-	}
-
-	// 빈 등록을 사용하는 예제. CommandLineRunner 인터페이스 구현과 동일한 동작
 	@Bean
-	fun printAppInfo() = CommandLineRunner {
-		println("[빈등록] 이름 : $appName")
-		println("[빈등록] 버전 : $appVersion")
-		println("[빈등록] 설명 : $appDescription")
+	fun init() = CommandLineRunner {
+		val appName = environment.getProperty("info.app.name")
+		val appVersion = environment.getProperty("info.app.version")
+		val appDescription = environment.getProperty("info.app.description")
+
+		print("앱 이름 : $appName\n")
+		print("앱 버전 : $appVersion\n")
+		print("앱 설명 : $appDescription")
 	}
 }
 
